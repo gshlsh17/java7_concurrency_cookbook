989ed09b5b326ccb871c51d73cc440b066625239
pss <pss@pss-PC>
2016-03-09 22:17:21 +0800

    chapter 1.13, thread factory

---
+++ src/main/java/chapter_1_13/Task.java
@@ -0,0 +1,15 @@
+package chapter_1_13;
+
+import java.util.concurrent.TimeUnit;
+
+public class Task implements Runnable {
+
+	public void run() {
+		try {
+			TimeUnit.SECONDS.sleep(1);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
